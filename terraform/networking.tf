2d7dcc772b46e3476d363071fc6cd0dedd72f1e1
preethambr30 <preethambr18@gmail.com>
2026-03-14 12:45:33 +0530

    Update networking.tf

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -1,1 +1,31 @@
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.devops_vpc.id
 
+  tags = {
+    Name = "devops-igw"
+  }
+}
+
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.devops_vpc.id
+
+  tags = {
+    Name = "devops-public-rt"
+  }
+}
+
+resource "aws_route" "internet_route" {
+  route_table_id         = aws_route_table.public_rt.id
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.igw.id
+}
+
+resource "aws_route_table_association" "subnet1_assoc" {
+  subnet_id      = aws_subnet.subnet1.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+resource "aws_route_table_association" "subnet2_assoc" {
+  subnet_id      = aws_subnet.subnet2.id
+  route_table_id = aws_route_table.public_rt.id
+}
