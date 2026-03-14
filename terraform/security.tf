c956bf51eba35c2324a0643f4b67c3519c6feaeb
preethambr30 <preethambr18@gmail.com>
2026-03-14 12:41:38 +0530

    Update security.tf

--- terraform/security.tf
+++ terraform/security.tf
@@ -1,1 +1,31 @@
+resource "aws_security_group" "devops_sg" {
+  vpc_id = aws_vpc.devops_vpc.id
 
+  ingress {
+    from_port = 22
+    to_port = 22
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 30000
+    to_port = 32767
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port = 80
+    protocol = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port = 0
+    to_port = 0
+    protocol = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
