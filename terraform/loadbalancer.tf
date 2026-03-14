14193d02caeb5d9f0b58f0b44510cee317875b0a
preethambr30 <preethambr18@gmail.com>
2026-03-14 12:45:49 +0530

    Update loadbalancer.tf

--- terraform/loadbalancer.tf
+++ terraform/loadbalancer.tf
@@ -1,1 +1,42 @@
+resource "aws_lb" "devops_alb" {
+  name               = "devops-dashboard-alb"
+  load_balancer_type = "application"
+  subnets = [
+    aws_subnet.subnet1.id,
+    aws_subnet.subnet2.id
+  ]
 
+  security_groups = [aws_security_group.devops_sg.id]
+
+  tags = {
+    Name = "devops-alb"
+  }
+}
+
+resource "aws_lb_target_group" "tg" {
+  name     = "devops-target-group"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.devops_vpc.id
+
+  health_check {
+    path                = "/"
+    protocol            = "HTTP"
+    matcher             = "200"
+    interval            = 30
+    timeout             = 5
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_lb.devops_alb.arn
+  port              = "80"
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tg.arn
+  }
+}
