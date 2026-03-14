a25fb84d4be3f73135a6271cb143f4471d2bac7e
preethambr30 <preethambr18@gmail.com>
2026-03-14 13:14:04 +0530

    Update networking.tf

--- terraform/networking.tf
+++ terraform/networking.tf
@@ -19,13 +19,3 @@ resource "aws_route" "internet_route" {
   destination_cidr_block = "0.0.0.0/0"
   gateway_id             = aws_internet_gateway.igw.id
 }
-
-resource "aws_route_table_association" "subnet1_assoc" {
-  subnet_id      = aws_subnet.subnet1.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-resource "aws_route_table_association" "subnet2_assoc" {
-  subnet_id      = aws_subnet.subnet2.id
-  route_table_id = aws_route_table.public_rt.id
-}
