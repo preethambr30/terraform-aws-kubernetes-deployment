3d15fc55145db9de01e54737ba12e23ba3b1d6ff
preethambr30 <preethambr18@gmail.com>
2026-03-14 13:37:12 +0530

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "preetham-terraform-devops-state"
+    bucket         = "preetham-devops-terraform-state"
     key            = "devops-project/terraform.tfstate"
     region         = "ap-south-1"
     encrypt        = true
